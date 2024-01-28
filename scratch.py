c6267f4d1248f5ff396cfa5ddcfafa55e5ff301b
Home <dsshvakin@mail.ru>
2024-01-28 19:02:24 +0300

    2

--- scratch.py
+++ scratch.py
@@ -1,20 +1,7 @@
-#ef genSet(clist):
-    #a = list(set(clist))
-    #return sorted(a)
-
-
-#if __name__ == "__main__":
-    #print
-    #genSet([5, 2, 8, 4, 1, 8])
-    #print
-    #genSet([3, -2, -4, -8, 6, -9, 0])
-
-a = []
-c = int(input("Сколько добавить чисел \n"))
-for i in range(0, c):
-    if c > 0:
-        a.append(input("Введите число \n"))
-    else:
-        exit()
-a.reverse()
-print(a)
+def inc(alist):
+  if len(alist) == 2:
+    return alist[0] <= alist[1]
+  if alist[0] > alist[1]:
+    return False
+  return inc(alist[1:])
+print(inc([1, 2, 5, 4, 8]))
